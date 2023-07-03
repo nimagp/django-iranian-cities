fa66828b7e30321e986eb24e4c27c84e8dc70233
nimagp <ngsgfa2@gmail.com>
2023-07-03 22:55:46 +0330

    Avoiding data duplication

--- iranian_cities/migrations/apply_data.py
+++ iranian_cities/migrations/apply_data.py
@@ -1,12 +1,14 @@
 #trying to bypass the use of management command 
 from django.core.management import call_command
 from django.db import migrations
-
+from django.db.migrations.recorder import MigrationRecorder
 def my_migration(apps, schema_editor):
     # Do some schema modifications here
-
-    # Call the management command
-    call_command('generate_city')
+    try:
+        MigrationRecorder.get(name__contains = 'apply_data')
+    except:
+        # Call the management command
+        call_command('generate_city')
 
 class Migration(migrations.Migration):
     dependencies = [
